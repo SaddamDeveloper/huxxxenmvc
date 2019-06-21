46cd3a1c0f6a77266c30a7fbde93daa6790276cb
saddamdeveloper <sishack8@gmail.com>
2019-06-21 15:44:03 +0530

    2nd commit

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -10,7 +10,23 @@
     protected $params = [];
 
     public function __construct(){
-      print_r($this->getUrl());
+      // print_r($this->getUrl());
+      $url = $this->getUrl();
+      //Look in controllers for first value
+      if(file_exists('../app/controllers/' . ucwords($url[0]) . '.php')){
+
+        //If exists set as controller
+        $this->currentController = ucwords($url[0]);
+
+        //unset 0 Index
+        unset($url[0]);
+      }
+
+      //Require the controller
+      require_once('../app/controllers/'. $this->currentController . '.php');
+
+      //Instantiate the controller
+      $this->currentController = new $this->currentController;
     }
 
     public function getUrl(){

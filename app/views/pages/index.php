38b8d3c1ad77dfd37c1233f8feb8709b86049dce
Saddam Hussain <36893768+SaddamDeveloper@users.noreply.github.com>
2019-06-22 15:50:26 +0530

    Update index.php

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,4 +1,4 @@
 <?php require APPROOT . './views/inc/header.php' ?>
     <h1><?php echo $data['title'] ?></h1>
-    <p>This is the HuxxxenMVC PHP Framework. Please refer  to the documentatin on how to use it.</p>
+    <p>This is the HuxxxenMVC PHP Framework. Please <a href="https://github.com/SaddamDeveloper/huxxxenmvc#server-requirements">refer</a> to the documentatin on how to use it.</p>
 <?php require APPROOT . '/views/inc/footer.php' ?>

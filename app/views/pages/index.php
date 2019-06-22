3c06245e6f195a6075ea92b176f65f051d0f8a24
saddamdeveloper <sishack8@gmail.com>
2019-06-22 15:39:11 +0530

    fixed bug

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,3 +1,4 @@
 <?php require APPROOT . './views/inc/header.php' ?>
     <h1><?php echo $data['title'] ?></h1>
+    <p>This is the HuxxxenMVC PHP Framework. Please refer  to the documentatin on how to use it.</p>
 <?php require APPROOT . '/views/inc/footer.php' ?>

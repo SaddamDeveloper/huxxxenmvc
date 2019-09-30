d1f87baba476086ad9ba79506bdabc760677f0f7
saddamdeveloper <sishack8@gmail.com>
2019-09-30 16:47:04 +0530

    Vue JS Integration

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,5 +1,7 @@
+<!-- jQuery Optional -->
 <script src="<?php echo URLROOT ?>/js/jQuery.min.js"></script>
-<script src="<?php echo URLROOT ?>/js/bootstrap.min.js"></script>
+<!-- Vue js Optional --> 
+<!-- <script src="<?php //echo URLROOT ?>/js/bootstrap.min.js"></script> -->
 <script src="<?php echo URLROOT ?>/js/main.js"></script>
 </body>
 </html>

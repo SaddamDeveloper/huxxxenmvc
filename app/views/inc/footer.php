b6a040f121d9d9cd2de2ad3aece89707cebf7e4b
saddamdeveloper <sishack8@gmail.com>
2019-09-30 17:10:10 +0530

    Update Vue JS

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,7 +1,8 @@
 <!-- jQuery Optional -->
 <script src="<?php echo URLROOT ?>/js/jQuery.min.js"></script>
 <!-- Vue js Optional --> 
-<!-- <script src="<?php //echo URLROOT ?>/js/vue.min.js"></script> -->
+<script src="<?php echo URLROOT ?>/js/vue.min.js"></script>
+
 <script src="<?php echo URLROOT ?>/js/bootstrap.min.js"></script>
 <script src="<?php echo URLROOT ?>/js/main.js"></script>
 </body>
